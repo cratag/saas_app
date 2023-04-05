5c11e1dbcdaf2fd1d28aca9b03c9896a3df9d7a4
Manuel Faner <devcratag@gmail.com>
2023-04-05 17:20:12 -0300

    Improve AWS S3 interface

--- lib/s3_client.rb
+++ lib/s3_client.rb
@@ -1,16 +1,19 @@
 require 'aws-sdk-s3'
 require 'erb'
 
+AWS_REGION = "us-east-1"
+
 class S3Client
   def initialize
     @client = Aws::S3::Client.new(
       access_key_id: Rails.application.credentials.aws_access_key_id,
       secret_access_key: Rails.application.credentials.aws_secret_access_key,
-      region: "us-east-1"
+      region: AWS_REGION
     )
   end
 
   def upload_file(artifact)
+    # Uploads a file to S3 bucket, and returns the URL
     begin
       file_extension = artifact.upload.original_filename.slice(artifact.upload.original_filename.index('.')..-1)
       @client.put_object(
@@ -21,7 +24,7 @@ class S3Client
       )
       "https://#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com/#{artifact.project.tenant.organization}/#{artifact.name}#{file_extension}"
     rescue => exception
-      puts exception
+      puts "# Error uploading artifact #{artifact.original_filename} to AWS S3: #{exception}"
       return nil
     end
   end
